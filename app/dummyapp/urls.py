894798e19a82ff4aec1f658013a4252b071e79b3
theonlyrealmax <max.crypto@hotmail.com>
2023-05-04 21:16:44 +0200

    Začetek dela na end-pointih #3

--- app/dummyapp/urls.py
+++ app/dummyapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("get_data", views.get_data, name="get_data"),
+    path("tree", views.get_tree, name="tree"),
+    path("make_prediction", views.make_prediction, name="make_prediction")
 ]
--- app/dummyapp/views.py
+++ app/dummyapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 # from dummyapp.models import DummyModel
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+import json
+import pandas as pd
+import pickle
+import numpy as np
+
 
 # Create your views here.
 
@@ -19,9 +27,53 @@ def get_data(response):
         uploaded_file = response.FILES["file"]
         print(uploaded_file.name)
 
+
+        df = pd.read_excel(uploaded_file)
+        y_new = df.iloc[:, 0]
+
+        loaded_model = pickle.load(open("dt_1.pickle", "rb"))
+
+        pred_data = np.array(y_new)
+        predicted = loaded_model.predict(pred_data)
+        predicted_list = predicted.tolist()
+
+        json_predicted = json.dumps(predicted_list)
+
+        return Response(json_predicted)
+
         # Save file to database
         # fileModel = DummyModel(file=uploaded_file)
         # fileModel.save()
-        return HttpResponse("File uploaded")
+        # return HttpResponse("File uploaded")
     else:
         return HttpResponse("File not uploaded")
+
+
+@api_view(['POST'])
+def make_prediction(request):
+    uploaded_file = request.FILES["file"]
+    df = pd.read_excel(uploaded_file)
+    y_new = df.iloc[:, 0]
+
+    loaded_model = pickle.load(open("./decision_models/dt_1.pickle", "rb"))
+
+    pred_data = np.array(y_new)
+    predicted = loaded_model.predict(pred_data)
+    predicted_list = predicted.tolist()
+
+    json_predicted = json.dumps(predicted_list)
+
+    return Response(json_predicted)
+
+
+@api_view(['GET'])
+def get_tree(request):
+    simple_object = {}
+
+    simple_object["name"] = "nekaj"
+    simple_object["test"] = 155
+    simple_object["list"] = [1, 2, 3, 4, 5]
+
+    json_simple = json.dumps(simple_object)
+
+    return Response(json_simple) # vrne JSON drevo
