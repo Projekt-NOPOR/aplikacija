3bc2c4aeeddec78c31ef02868c6af59c3af306b0
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2023-05-25 22:45:47 +0200

    Adding additional electron app config

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require('path');
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, protocol } = require('electron');
 
-const { PythonShell } = require('python-shell')
+const { PythonShell } = require('python-shell');
 
 const djangoServer = new PythonShell('../app/manage.py', {
     args: ['runserver', '127.0.0.1:8000']
-})
+});
 
 djangoServer.on('message', (message) => {
     console.log('Django server:', message);
@@ -17,19 +17,17 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
+            nodeIntegration: false,
+            contextIsolation: true,
         },
-    })
+    });
 
-    win.loadURL(
-        `file://${__dirname}/../app/dummyapp/dummyapp/build/index.html`
-    );
-}
+    win.loadURL(`file://${__dirname}/../app/dummyapp/dummyapp/build/index.html`);
+};
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
-})
+});
 
 app.whenReady().then(() => {
     createWindow()
@@ -37,4 +35,4 @@ app.whenReady().then(() => {
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
-})
+});
