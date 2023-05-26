33963e7ea04cbb7c573b391eecedd71b46a6ece7
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2023-05-26 14:04:21 +0200

    neki

--- electron-app/main.js
+++ electron-app/main.js
@@ -19,7 +19,6 @@ const createWindow = () => {
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
-            webSecurity: false,
         },
     });
 
